976890892151cfd6998ac2a6090367afe9fbf51d
Hanna Stseliukova <70797548+ByHannaStseliukova@users.noreply.github.com>
2024-09-04 14:19:36 +0000

    Init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let stack = [];
+ 
+  for (let i=0; i<str.length; i++) {
+  let currentSymbol = str[i];
+  let bracketsController = bracketsConfig.find(cnf => cnf.includes(currentSymbol));
+    if (!bracketsController) {
+      return false;
+    }
+  const [open, close] = bracketsController;
+    if (open === close) {
+      if (close === stack[stack.length-1]) {
+        stack.pop();
+      } 
+      else {
+        stack.push(currentSymbol);
+      }
+    } 
+    else {
+      if (open === currentSymbol) {
+        stack.push(currentSymbol);
+      } 
+      else if (close === currentSymbol && stack[stack.length-1] === open) {
+        stack.pop();
+      } 
+      else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
